460f3e067e05124dd986f3faf61f800bd733b5a0
szczypiorofix <Scypiur1>
2017-09-04 08:11:05 +0200

    Przesiadka z API fixer.io na openexchangerates.

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -42,7 +42,11 @@ var currencyCalc = {
         this.htmlElements.inputValue = document.getElementById('inputValue');
         this.htmlElements.currencySelect = document.getElementById('currencySelect');
         
-        this.getJson('https://api.fixer.io/latest');
+        //this.getJson('https://api.fixer.io/latest');
+        this.getJson('https://openexchangerates.org/api/latest.json?app_id=API_KEY');
+
+        // Dane z API pobierać za pomocą skryptu PHP, API_KEY umieścić w pliku .ini. Wykorzystać klasę Config.php do pobierania
+        // danych z API a do skryptu JS zwracać już gotowe dane.
         
         this.htmlElements.inputValue.addEventListener('input', function() {
             if (this.value !== '' && this.value !== null && this.value > 0) {
@@ -67,15 +71,16 @@ var currencyCalc = {
      * @returns {Array|currencyCalc.recalc.temp} a new array with recalculated currency rates.
      */
     recalc: function(current, amount) {
-        let b;
+        let b = 0;
+        let i = 0;
         let temp = [];
-        for (var i = 0; i < this.rates.length; i++) {
+        for (i = 0; i < this.rates.length; i++) {
             if (this.rates[i].cur === current) {
                 b = this.rates[i];
             }
         }
         let tempVal = 0;
-        for (var i = 0; i < this.rates.length; i++) {
+        for (i = 0; i < this.rates.length; i++) {
             tempVal = (1 / this.rates[i].val);
             temp.push({'cur': this.rates[i].cur, 'val': (1 / (tempVal * b.val) ) * amount});
         }
@@ -259,6 +264,177 @@ var currencyCalc = {
         {country: 'Węgry', code: 'HUF', name: 'Forint'},
         {country: 'Wielka Brytania', code: 'GBP', name: 'Funt'}
     ]
+
+/*     {
+        "AED": "United Arab Emirates Dirham",
+        "AFN": "Afghan Afghani",
+        "ALL": "Albanian Lek",
+        "AMD": "Armenian Dram",
+        "ANG": "Netherlands Antillean Guilder",
+        "AOA": "Angolan Kwanza",
+        "ARS": "Argentine Peso",
+        "AUD": "Australian Dollar",
+        "AWG": "Aruban Florin",
+        "AZN": "Azerbaijani Manat",
+        "BAM": "Bosnia-Herzegovina Convertible Mark",
+        "BBD": "Barbadian Dollar",
+        "BDT": "Bangladeshi Taka",
+        "BGN": "Bulgarian Lev",
+        "BHD": "Bahraini Dinar",
+        "BIF": "Burundian Franc",
+        "BMD": "Bermudan Dollar",
+        "BND": "Brunei Dollar",
+        "BOB": "Bolivian Boliviano",
+        "BRL": "Brazilian Real",
+        "BSD": "Bahamian Dollar",
+        "BTC": "Bitcoin",
+        "BTN": "Bhutanese Ngultrum",
+        "BWP": "Botswanan Pula",
+        "BYN": "Belarusian Ruble",
+        "BZD": "Belize Dollar",
+        "CAD": "Canadian Dollar",
+        "CDF": "Congolese Franc",
+        "CHF": "Swiss Franc",
+        "CLF": "Chilean Unit of Account (UF)",
+        "CLP": "Chilean Peso",
+        "CNH": "Chinese Yuan (Offshore)",
+        "CNY": "Chinese Yuan",
+        "COP": "Colombian Peso",
+        "CRC": "Costa Rican Colón",
+        "CUC": "Cuban Convertible Peso",
+        "CUP": "Cuban Peso",
+        "CVE": "Cape Verdean Escudo",
+        "CZK": "Czech Republic Koruna",
+        "DJF": "Djiboutian Franc",
+        "DKK": "Danish Krone",
+        "DOP": "Dominican Peso",
+        "DZD": "Algerian Dinar",
+        "EGP": "Egyptian Pound",
+        "ERN": "Eritrean Nakfa",
+        "ETB": "Ethiopian Birr",
+        "EUR": "Euro",
+        "FJD": "Fijian Dollar",
+        "FKP": "Falkland Islands Pound",
+        "GBP": "British Pound Sterling",
+        "GEL": "Georgian Lari",
+        "GGP": "Guernsey Pound",
+        "GHS": "Ghanaian Cedi",
+        "GIP": "Gibraltar Pound",
+        "GMD": "Gambian Dalasi",
+        "GNF": "Guinean Franc",
+        "GTQ": "Guatemalan Quetzal",
+        "GYD": "Guyanaese Dollar",
+        "HKD": "Hong Kong Dollar",
+        "HNL": "Honduran Lempira",
+        "HRK": "Croatian Kuna",
+        "HTG": "Haitian Gourde",
+        "HUF": "Hungarian Forint",
+        "IDR": "Indonesian Rupiah",
+        "ILS": "Israeli New Sheqel",
+        "IMP": "Manx pound",
+        "INR": "Indian Rupee",
+        "IQD": "Iraqi Dinar",
+        "IRR": "Iranian Rial",
+        "ISK": "Icelandic Króna",
+        "JEP": "Jersey Pound",
+        "JMD": "Jamaican Dollar",
+        "JOD": "Jordanian Dinar",
+        "JPY": "Japanese Yen",
+        "KES": "Kenyan Shilling",
+        "KGS": "Kyrgystani Som",
+        "KHR": "Cambodian Riel",
+        "KMF": "Comorian Franc",
+        "KPW": "North Korean Won",
+        "KRW": "South Korean Won",
+        "KWD": "Kuwaiti Dinar",
+        "KYD": "Cayman Islands Dollar",
+        "KZT": "Kazakhstani Tenge",
+        "LAK": "Laotian Kip",
+        "LBP": "Lebanese Pound",
+        "LKR": "Sri Lankan Rupee",
+        "LRD": "Liberian Dollar",
+        "LSL": "Lesotho Loti",
+        "LYD": "Libyan Dinar",
+        "MAD": "Moroccan Dirham",
+        "MDL": "Moldovan Leu",
+        "MGA": "Malagasy Ariary",
+        "MKD": "Macedonian Denar",
+        "MMK": "Myanma Kyat",
+        "MNT": "Mongolian Tugrik",
+        "MOP": "Macanese Pataca",
+        "MRO": "Mauritanian Ouguiya",
+        "MUR": "Mauritian Rupee",
+        "MVR": "Maldivian Rufiyaa",
+        "MWK": "Malawian Kwacha",
+        "MXN": "Mexican Peso",
+        "MYR": "Malaysian Ringgit",
+        "MZN": "Mozambican Metical",
+        "NAD": "Namibian Dollar",
+        "NGN": "Nigerian Naira",
+        "NIO": "Nicaraguan Córdoba",
+        "NOK": "Norwegian Krone",
+        "NPR": "Nepalese Rupee",
+        "NZD": "New Zealand Dollar",
+        "OMR": "Omani Rial",
+        "PAB": "Panamanian Balboa",
+        "PEN": "Peruvian Nuevo Sol",
+        "PGK": "Papua New Guinean Kina",
+        "PHP": "Philippine Peso",
+        "PKR": "Pakistani Rupee",
+        "PLN": "Polish Zloty",
+        "PYG": "Paraguayan Guarani",
+        "QAR": "Qatari Rial",
+        "RON": "Romanian Leu",
+        "RSD": "Serbian Dinar",
+        "RUB": "Russian Ruble",
+        "RWF": "Rwandan Franc",
+        "SAR": "Saudi Riyal",
+        "SBD": "Solomon Islands Dollar",
+        "SCR": "Seychellois Rupee",
+        "SDG": "Sudanese Pound",
+        "SEK": "Swedish Krona",
+        "SGD": "Singapore Dollar",
+        "SHP": "Saint Helena Pound",
+        "SLL": "Sierra Leonean Leone",
+        "SOS": "Somali Shilling",
+        "SRD": "Surinamese Dollar",
+        "SSP": "South Sudanese Pound",
+        "STD": "São Tomé and Príncipe Dobra",
+        "SVC": "Salvadoran Colón",
+        "SYP": "Syrian Pound",
+        "SZL": "Swazi Lilangeni",
+        "THB": "Thai Baht",
+        "TJS": "Tajikistani Somoni",
+        "TMT": "Turkmenistani Manat",
+        "TND": "Tunisian Dinar",
+        "TOP": "Tongan Pa'anga",
+        "TRY": "Turkish Lira",
+        "TTD": "Trinidad and Tobago Dollar",
+        "TWD": "New Taiwan Dollar",
+        "TZS": "Tanzanian Shilling",
+        "UAH": "Ukrainian Hryvnia",
+        "UGX": "Ugandan Shilling",
+        "USD": "United States Dollar",
+        "UYU": "Uruguayan Peso",
+        "UZS": "Uzbekistan Som",
+        "VEF": "Venezuelan Bolívar Fuerte",
+        "VND": "Vietnamese Dong",
+        "VUV": "Vanuatu Vatu",
+        "WST": "Samoan Tala",
+        "XAF": "CFA Franc BEAC",
+        "XAG": "Silver Ounce",
+        "XAU": "Gold Ounce",
+        "XCD": "East Caribbean Dollar",
+        "XDR": "Special Drawing Rights",
+        "XOF": "CFA Franc BCEAO",
+        "XPD": "Palladium Ounce",
+        "XPF": "CFP Franc",
+        "XPT": "Platinum Ounce",
+        "YER": "Yemeni Rial",
+        "ZAR": "South African Rand",
+        "ZMW": "Zambian Kwacha",
+        "ZWL": "Zimbabwean Dollar"
+      } */
 };
 
 document.addEventListener("DOMContentLoaded", function(event) {
