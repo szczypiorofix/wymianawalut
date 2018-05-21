0dd860cf7af5ffc680a6aeb43d1415f81f3862a8
szczypiorofix <Scypiur1>
2018-05-22 00:46:26 +0200

    Poprawka błędu złej ścieżki dostępu do config.ini

--- update.php
+++ update.php
@@ -6,7 +6,7 @@
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 
-    define('CONFIG_FILE', '../config.ini');
+    define('CONFIG_FILE', 'config.ini');
 
     require_once 'config.php';
 
